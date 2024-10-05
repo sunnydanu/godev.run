9609c412381e36e19355908802a09e5388b53690
sunnydanu <mail@sunnydanu.com>
2024-10-05 19:25:45 +0530

    refector(file-transfer): icon updated

--- src/tools/peer-share/index.ts
+++ src/tools/peer-share/index.ts
@@ -1,4 +1,4 @@
-import { ArrowsShuffle } from '@vicons/tabler';
+import { Share } from '@vicons/tabler';
 import { defineTool } from '../tool';
 
 export const tool = defineTool({
@@ -7,6 +7,6 @@ export const tool = defineTool({
   description: 'PeerShare - A client-side secure P2P file sharing using WebRTC',
   keywords: ['send-files', 'file-transfer', 'file share', 'file sharing', 'P2P file sharing'],
   component: () => import('./peer-share.vue'),
-  icon: ArrowsShuffle,
+  icon: Share,
   createdAt: new Date('2024-09-26'),
 });
