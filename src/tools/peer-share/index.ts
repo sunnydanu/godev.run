fdcb89882f28c0af7e224b5b40aec5f649ad66c0
sunnydanu <mail@sunnydanu.com>
2024-10-20 22:03:15 +0530

    chore(peer-share): theme update

--- src/tools/peer-share/index.ts
+++ src/tools/peer-share/index.ts
@@ -5,7 +5,7 @@ export const tool = defineTool({
   name: 'Peer Share',
   path: '/peer-share/',
   description: 'PeerShare: A secure client-side P2P connection offering features like file sharing and live clipboard.',
-  keywords: ['send-files', 'file share', 'file sharing', 'Peer-to-Peer (P2P)', 'Secure Connection', 'Client-Side', 'File Sharing', 'Live Clipboard', 'Data Transfer', 'Collaboration', 'Real-Time Communication'],
+  keywords: ['send', 'send files', 'file share', 'file', 'p2p', 'File Sharing', 'Live Clipboard', 'Data Transfer', 'Collaboration', 'Real-Time Communication'],
   component: () => import('./peer-share.vue'),
   icon: Share,
   createdAt: new Date('2024-09-26'),
