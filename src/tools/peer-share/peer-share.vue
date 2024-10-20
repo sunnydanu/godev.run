89c80958e2e1ec0641a2154ccefc18bb87505dbc
sunnydanu <mail@sunnydanu.com>
2024-10-20 22:03:15 +0530

    chore(peer-share): theme update

--- src/tools/peer-share/peer-share.vue
+++ src/tools/peer-share/peer-share.vue
@@ -52,9 +52,117 @@ onMounted(() => {
 </template>
 
 <style lang="less">
-.peerShareContainer button.btn.btn-primary,.alert.alert-info {
-    background: #1ea54c;
-    border-color: #1ea54c;
-    color: rgba(255, 255, 255, 0.82);
+.peerShareContainer button.btn.btn-secondary,.peerShareContainer label.btn{
+    line-height: 1;
+    font-family: inherit;
+    font-size: 14px;
+    border: none;
+    text-align: center;
+    cursor: pointer;
+    text-decoration: none;
+    font-weight: 400;
+    color:  #ffffffd1;
+    padding: 0 14px;
+    transition: background-color cubic-bezier(0.4, 0, 0.2, 1) 0.3s;
+    background-color: rgba(255, 255, 255, 0.08);
+    display: inline-flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+
+}
+
+.peerShareContainer button.btn.btn-primary{
+    line-height: 1;
+    font-family: inherit;
+    font-size: 14px;
+    border: none;
+    text-align: center;
+    cursor: pointer;
+    text-decoration: none;
+    font-weight: 400;
+    color: #1ea54c;
+    padding: 0 14px;
+    transition: background-color cubic-bezier(0.4, 0, 0.2, 1) 0.3s;
+    background-color: #18a0582f;
+    display: inline-flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+
+}
+ .peerShareContainer .alert.alert-info, .peerShareContainer .alert.alert-success {
+    line-height: 1;
+    font-family: inherit;
+    font-size: 14px;
+    border: none;
+    text-align: center;
+    cursor: pointer;
+    text-decoration: none;
+    font-weight: 400;
+    color: #1ea54c;
+    transition: background-color cubic-bezier(0.4, 0, 0.2, 1) 0.3s;
+    background-color: #18a0582f;
+    align-items: center;
+    justify-content: center;
+    flex-shrink: 0;
+
+}
+
+.peerShareContainer button.btn.btn-secondary:not(.disabled):active,
+.peerShareContainer button.btn.btn-ghost:active,
+.peerShareContainer label.btn:active {
+    background-color: rgba(255, 255, 255, 0.24);
+}
+
+.peerShareContainer .btn:not(.btn-error):hover, .peerShareContainer .btn:focus,
+.peerShareContainer .input:hover, .peerShareContainer input:focus,
+.peerShareContainer .select:hover, .peerShareContainer .select:focus ,
+.peerShareContainer option:hover,.peerShareContainer option:focus
+  {
+    outline: 1px solid #1ea54c;
+}
+.peerShareContainer button.btn.btn-secondary:focus,
+.peerShareContainer button.btn.btn-secondary:hover,
+.peerShareContainer button.btn.btn-ghost:focus,
+.peerShareContainer button.btn.btn-ghost:hover,
+.peerShareContainer label.btn:hover{
+   background-color: rgba(255, 255, 255, 0.12)
+}
+
+.peerShareContainer button.btn.btn-primary:hover,
+.peerShareContainer button.btn.btn-primary:focus,
+.peerShareContainer .alert.alert-success:focus,
+.peerShareContainer .alert.alert-info:focus{
+    outline: 1px solid #1ea54c;
+}
+
+.peerShareContainer button.btn.btn-primary:not(.disabled):active,
+.peerShareContainer button.btn.btn-primary:not(.disabled):hover,
+.peerShareContainer .alert.alert-success:hover,
+.peerShareContainer .alert.alert-info:hover{
+    background-color: #00823a2f;
+}
+
+div.indicator .btn{
+  background-color: inherit,
+}
+
+.peerShareContainer .btn.btn-error ,.peerShareContainer .alert.alert-error{
+    background-color: #773138;
+    color: #ff6877;
+    outline-color: #773138;
+    border-color: #773138;
+}
+
+.peerShareContainer .btn.btn-error:hover, .peerShareContainer .btn.btn-error:focus, .peerShareContainer .btn.btn-error:active{
+    background-color: #772931;
+    outline-color: #ff6877;
+    border-color: #ff6877;
+}
+.peerShareContainer .border-black{
+  border-color: #1ea54c;
 }
 </style>
